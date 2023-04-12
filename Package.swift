74763c27e001982bef7399c71d07c67a725d39f3
Yury <davey.jones.music@gmail.com>
2023-04-12 16:42:24 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Python-iOS",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v9), .tvOS(.v13)],
     products: [
         .library(
             name: "Python-iOS",
